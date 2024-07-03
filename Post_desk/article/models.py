4ac78b25abe9d7c872ffa26c50863bc9ad936189
Slava-inc <v.danchenko@voshod.local>
2024-07-03 15:21:32 +0300

    article index & detail in ckeditor

--- Post_desk/article/models.py
+++ Post_desk/article/models.py
@@ -9,19 +9,18 @@ from ckeditor_uploader.fields import RichTextUploadingField
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
 
 class Article(models.Model):
     tittle = models.CharField(max_length=255)
+    text = models.CharField(max_length=1024, default='text edition')
     content = RichTextField(blank=True, null=True)
     content_upload = RichTextUploadingField(blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    likes = models.ManyToManyField(User, through='UserArticle', related_name='user_like') 
-    category = models.ManyToManyField(Category, through='CategoryArticle')
-
-
-class CategoryArticle(models.Model):
+    likes = models.ManyToManyField(User, through='UserArticle', related_name='user_like', blank=True) 
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+
 
 class UserArticle(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
